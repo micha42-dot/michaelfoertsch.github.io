f9385fbf972ce32c6bdfe9f36159ce416c1a27f6
micha42-dot <80447964+micha42-dot@users.noreply.github.com>
2025-11-19 04:25:54 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Da dein Repo michaelfoertsch.github.io heißt, ist die Base URL '/'
-  base: '/',
+  // './' sorgt dafür, dass Pfade relativ sind. Das löst fast alle "Datei nicht gefunden" Probleme.
+  base: './',
 });
